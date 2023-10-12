ed520c8a20546fad33595c0da2ac43f7308b080f
kwasikwarteng <megakwasikwarteng@gmail.com>
2023-10-12 22:08:13 +0000

    Initial Commit of Blog API

---
+++ BlogAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
